382396f7fe5aec30791ced8db71580de4f3ef363
william <william.f.bowden@gmail.com>
2021-05-09 18:55:54 -0700

    Made a requirements folder. Created test cases for authentication class, price info streaming class, get macd index class.

--- src/app/main.py
+++ src/app/main.py
@@ -1,21 +1,76 @@
 import datetime
+import json
 
-import pandas_datareader as web
+import httpx
+from tda import auth, client
+from tda.auth import easy_client
+from tda.client import Client
+
+from src.app import config
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+# macd imports
+import pandas as pd
+import numpy as np
+from datetime import datetime
+import matplotlib.pyplot as plt
+import pyEX as p
+from iexfinance.refdata import get_symbols
 
 
-class main:
-    tda_apikey = "DRBKZQKGIVCYUYD83VGHXDMUVKKX3TDR"
+class app_authenticator:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def authenticate():
+        try:
+            c = auth.client_from_token_file(config.TOKEN_PATH, config.API_KEY)
+        except FileNotFoundError:
+            with webdriver.Chrome(ChromeDriverManager().install()) as driver:
+                c = auth.client_from_login_flow(
+                    driver, config.API_KEY, config.REDIRECT_URI, config.TOKEN_PATH)
+
+
+class stream_option_data:
+    def __init__(self):
+        pass
+
+    def get_label_price_histoy(self, LABEL: str):
+        c = easy_client(
+            api_key=config.API_KEY,
+            redirect_uri=config.REDIRECT_URI,
+            token_path=config.TOKEN_PATH)
+
+        resp = c.get_price_history(LABEL,
+                                   period_type=Client.PriceHistory.PeriodType.DAY,
+                                   period=Client.PriceHistory.Period.ONE_DAY,
+                                   frequency_type=Client.PriceHistory.FrequencyType.MINUTE,
+                                   frequency=Client.PriceHistory.Frequency.EVERY_FIVE_MINUTES)
+        assert resp.status_code == httpx.codes.OK
+        history = resp.json()
+        return history
+
+
+class option_strategy_executor:
 
     def __init__(self):
         pass
 
-    def auth(self):
+    def get_macd(self, label: str):
+        ticker = label
+        timeframe = '6m'
+        df = p.chartDF(ticker, timeframe, token="sk_335abdd65af64ef49f15e8ccef80bc84")
+        df = df[['close']]
+        df.reset_index(level=0, inplace=True)
+        df.columns = ['ds', 'y']
+        plt.plot(df.ds, df.y, label=label)
+        plt.show()
+
+
+class analysis_genesis:
+    def __init__(self):
         pass
 
-    # requests require user and apikey authorization
-
-    def display_market_data(self):
-        start = datetime.datetime(2019, 1, 1)
-        end = datetime.datetime(2019, 1, 10)
-        facebook = web.DataReader("FB", 'morningstar', start, end)
-        print(facebook.head())
+    # set chart to 5 minute candles
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,28 +1,38 @@
 import pytest
 
-from src.app.main import main
+from src.app.main import app_authenticator, stream_option_data, option_strategy_executor, analysis_genesis
 
 
 @pytest.fixture
-def tradebot():
-    tradebot: main = main()
-    return tradebot
+def setUp_App():
+    app: app_authenticator = app_authenticator()
+    return app
 
 
-def test_auth(tradebot):
+def test_authenticate(setUp_App):
+    # authenticate
+    setUp_App.authenticate()
     assert True
 
 
-def test_display_market_data(tradebot):
-    tradebot.display_market_data()
+def test_stream_data(setUp_App):
+    # stream data
+    order_maker: stream_option_data = stream_option_data()
+    # get price history for AAPL
+    print(str(order_maker.get_label_price_histoy('AAPL')))
     assert True
 
 
-def test_buy_calls(tradebot):
-    assert True
+def test_analyze_json():
+    analyzer: analysis_genesis = analysis_genesis()
 
 
-def test_exit_position_after_one_hour(tradebot):
-    assert True
+def test_get_macd_index():
+    # option bot
+    option_bot: option_strategy_executor = option_strategy_executor()
+    option_bot.get_macd('AAPL')
+    pass
+
+
 
 
